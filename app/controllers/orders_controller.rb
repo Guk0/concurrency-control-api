d51e363fb1a4d9c7373ed546d02b069249b57552
guk0 <abcgy39@naver.com>
2022-08-22 01:02:04 +0900

    add redlock logger & fix retry of redlock

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,28 +1,27 @@
 class OrdersController < ApplicationController
   def create
-    # begin
-    #   item = Item.find(params[:item_id])
-    #   block_result = $lock_manager.lock!("items#{item.id}", 2000) do
-    #     if item.count > 0
-    #       puts "Item #{item.id} remain #{item.count}"
-    #       item.decrement!(:count)
-    #       item.save          
-    #     else
-    #       puts "Item #{item.id} is out of stock"
-    #     end
-    #   end
-    # rescue Redlock::LockError
-    #   puts "lock error"
-    # end
-
-    item = Item.find(params[:item_id])
-    if item.count > 0
-      puts "item.count #{item.count}"
-      item.decrement!(:count)
-      item.save
-    else
-      puts "item.count 0"
+    $redlock_logger.info "get request"
+    begin
+      block_result = $lock_manager.lock!("items#{params[:item_id]}", 2000) do
+        item = Item.find(params[:item_id])
+        if item.count > 0
+          $redlock_logger.info "Item #{item.id} remain #{item.count}"
+          item.update(count: item.count - 1)
+        else
+          $redlock_logger.info "Item #{item.id} is out of stock"
+        end
+      end
+    rescue Redlock::LockError => e
+      $redlock_logger.info "lock error"
     end
-    render json: item
+
+    # item = Item.find(params[:item_id])
+    # if item.count > 0
+    #   puts "item.count #{item.count}"
+    #   item.update(count: item.count - 1)
+    # else
+    #   puts "item.count 0"
+    # end
+    # render json: item
   end
 end
--- config/initializers/redlock.rb
+++ config/initializers/redlock.rb
@@ -1,1 +1,12 @@
-$lock_manager = Redlock::Client.new(["redis://127.0.0.1:6379/0"])
+$lock_manager = Redlock::Client.new(
+  ["redis://redis:6379/0"], {
+    retry_count:   15,
+    retry_delay:   300, # milliseconds
+    retry_jitter:  200,  # milliseconds
+    redis_timeout: 10  # seconds
+  }
+)
+
+log_file_path = "#{Rails.root}/log/#{Rails.env}_redlock.log"
+$redlock_logger = Logger.new(log_file_path)
+
